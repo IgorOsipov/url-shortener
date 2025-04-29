dd5345b12098a58e5e491ec8290121b65be6c292
IgorOsipov <awesomeigor@gmail.com>
2025-04-29 10:41:32 +0300

    Implement logging setup in main.go based on environment configuration

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,19 +1,43 @@
 package main
 
 import (
-	"fmt"
 	"iosipoff/url-shortener/cmd/url-shortener/internal/config"
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
-	fmt.Printf("config: %+v", cfg)
 
-	//TODO: init logger: slog
+	log := setupLoger(cfg.Env)
+
+	log.Info("starting url-shortener", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
 
 	//TODO: init storage: sqlite
 
 	//TODO: init router: chi, chi render
 
 	//TODO: run server
+}
+
+func setupLoger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
 }
